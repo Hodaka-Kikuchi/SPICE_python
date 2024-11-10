c0c2fea4c0bb0645a7a6125abc0320f9ef67fef9
Hodaka <hodaka.kikuchi@gamil.com>
2024-11-10 16:25:33 +0900

    UBcalc

--- UBcalc.py
+++ UBcalc.py
@@ -4,14 +4,11 @@ import numpy as np
 
 def UB_calc(sp1,sp2,astar,bstar,cstar):
     u1 = sp1[0]*astar+sp1[1]*bstar+sp1[2]*cstar
-    U1 = u1 / np.sqrt(u1.T*u1)
-    print(U1)
+    U1 = u1 / np.sqrt(np.dot(u1, u1))
     u2 = sp2[0]*astar+sp2[1]*bstar+sp2[2]*cstar
-    uu2 = u2 - U1 * (U1.T*u2)
-    U2 = uu2 / np.sqrt(uu2.T*uu2)
-    print(U2)
+    uu2 = u2 - np.dot(U1, u2) * U1
+    U2 = uu2 / np.linalg.norm(uu2)
     U3=[U1[1]*U2[2]-U1[2]*U2[1], U1[2]*U2[0]-U1[0]*U2[2], U1[0]*U2[1]-U1[1]*U2[0]]
-    
     
     # 結果を返す
     return U1,U2,U3
