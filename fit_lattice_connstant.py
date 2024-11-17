73ce875fa1859a83b97a36c81576e04f2c91abb9
Hodaka <hodaka.kikuchi@gamil.com>
2024-11-17 12:18:28 +0900

    revise

---
+++ fit_lattice_connstant.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import numpy as np
+import math
+
+# fitting on lattice constant
+
+# cubic
+# tetragonal
+# hexagonal
+# orthorhombic
+# monoclinic
+# triclinic
+
+
+
+
+
+
+
+
+
+
+
+
