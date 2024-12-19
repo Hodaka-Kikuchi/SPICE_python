905aa58f75a6bad9ae1eab9308974edb70cfa831
Hodaka <hodaka.kikuchi@gamil.com>
2024-12-19 17:18:38 +0900

    hardware limit action

--- TriAxionSim.py
+++ TriAxionSim.py
@@ -1,5 +1,4 @@
 #cd C:\DATA_HK\python\SPICE_python
-
 # 右上にバージョン情報を表示
 __version__ = '1.4.3'
 """
@@ -1134,9 +1133,45 @@ def calculate_angle():
     angletable,error_message = on_anglecalc()
     #print("RLtable:", RLtable)
     
+    # INIファイルから設定を読み込む
+    config = configparser.ConfigParser()
+    # .exe化した場合に対応する
+    if getattr(sys, 'frozen', False):
+        # .exeの場合、sys.argv[0]が実行ファイルのパスになる
+        ini_path = os.path.join(os.path.dirname(sys.argv[0]), 'config.ini')
+    else:
+        # .pyの場合、__file__がスクリプトのパスになる
+        ini_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config.read(ini_path)
+    
+    monochromator_radius = float(config['settings']['monochromator_radius'])
+    monochromator_to_sample = float(config['settings']['monochromator_to_sample'])
+    sample_goniometer_radius = float(config['settings']['sample_goniometer_radius'])
+    sample_to_analyzer = float(config['settings']['sample_to_analyzer'])
+    analyzer_radius = float(config['settings']['analyzer_radius'])
+    analyzer_to_detector = float(config['settings']['analyzer_to_detector'])
+    detector_radius = float(config['settings']['detector_radius'])
+    floor_length = float(config['settings']['floor_length'])
+    floor_width = float(config['settings']['floor_width'])
+    floor_position_x = float(config['settings']['floor_position_x'])
+    floor_position_y = float(config['settings']['floor_position_y'])
+    
+    # sample gonioがfloorからはみ出る場合
+    positionY_sample = monochromator_to_sample * np.sin(np.radians(angletable['A1'])) - sample_goniometer_radius # < floor_position_y
+    # analyzer dramがfloorからはみ出る場合
+    positionY_analyzer = monochromator_to_sample * np.sin(np.radians(angletable['A1'])) - sample_to_analyzer * np.sin(np.radians(angletable['A2'] - angletable['A1'])) - analyzer_radius # < floor_position_y
+    # detector dramがfloorからはみ出る場合
+    positionY_detector = monochromator_to_sample * np.sin(np.radians(angletable['A1'])) - sample_to_analyzer * np.sin(np.radians(angletable['A2'] - angletable['A1'])) + analyzer_to_detector * np.sin(np.radians(angletable['A3'] - angletable['A2'] + angletable['A1'])) - detector_radius # < floor_position_y
+    positionX_detector = monochromator_to_sample * np.cos(np.radians(angletable['A1'])) + sample_to_analyzer * np.cos(np.radians(angletable['A2'] - angletable['A1'])) + analyzer_to_detector * np.cos(np.radians(angletable['A3'] - angletable['A2'] + angletable['A1'])) - detector_radius # < floor_position_x+floor_length
+    
     if error_message is not None:
         acl10.config(text=error_message, fg="red")
         return  # 計算を中断
+    elif (positionY_sample < floor_position_y or
+          positionY_analyzer < floor_position_y or
+          positionY_detector < floor_position_y or
+          positionX_detector > floor_position_x+floor_length):
+        acl10.config(text="Out of the floor.", fg="red")
     elif (round(angletable['C1'],4)<float(hwl2f.get()) or
         round(angletable['C1'],4)>float(hwl2t.get()) or
         round(angletable['A1'],4)<float(hwl3f.get()) or
@@ -1154,6 +1189,7 @@ def calculate_angle():
         round(angletable['nu'],4)<float(hwl9f.get()) or
         round(angletable['nu'],4)>float(hwl9t.get())):
         acl10.config(text="Out of hardware limit range.", fg="red")
+    
     else:
         # `phi_cal` の結果を用いた続きの処理
         acl10.config(text="The calculation was successful.", fg="black")
@@ -1805,13 +1841,36 @@ def constQscan_show_table():
     global angletable2
     angletable2 = angle_calc2(astar, bstar, cstar, U,B,UB, bpe, bpc2, bpmu, bpnu, bp, fixe, hw_ini, hw_fin, hw_inc, h_cal, k_cal, l_cal)
     
+    # 先にフロアの条件を読み込み
+    # INIファイルから設定を読み込む
+    config = configparser.ConfigParser()
+    # .exe化した場合に対応する
+    if getattr(sys, 'frozen', False):
+        # .exeの場合、sys.argv[0]が実行ファイルのパスになる
+        ini_path = os.path.join(os.path.dirname(sys.argv[0]), 'config.ini')
+    else:
+        # .pyの場合、__file__がスクリプトのパスになる
+        ini_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config.read(ini_path)
+    
+    monochromator_radius = float(config['settings']['monochromator_radius'])
+    monochromator_to_sample = float(config['settings']['monochromator_to_sample'])
+    sample_goniometer_radius = float(config['settings']['sample_goniometer_radius'])
+    sample_to_analyzer = float(config['settings']['sample_to_analyzer'])
+    analyzer_radius = float(config['settings']['analyzer_radius'])
+    analyzer_to_detector = float(config['settings']['analyzer_to_detector'])
+    detector_radius = float(config['settings']['detector_radius'])
+    floor_length = float(config['settings']['floor_length'])
+    floor_width = float(config['settings']['floor_width'])
+    floor_position_x = float(config['settings']['floor_position_x'])
+    floor_position_y = float(config['settings']['floor_position_y'])
+    
     A_sets = []  # A_setsリストを初期化
     QE_sets = []
     # resultsリストの各結果をTreeviewに追加
     for results in angletable2:
         values = tuple(results.values())
-        tree.insert("", "end", values=values)
-    
+        item_id = tree.insert("", "end", values=values)
         # A1, A2, A3 を取得して A_sets に追加
         A1 = round(results['A1'], 4)  # 'A1'
         A2 = -round(results['A2'], 4)  # 'A2'
@@ -1823,7 +1882,44 @@ def constQscan_show_table():
         k = round(results['k'], 4)  # 'A3'
         l = round(results['l'], 4)  # 'A3'
         QE_sets.append([hw, h, k,l])
-
+        
+        # sample gonioがfloorからはみ出る場合
+        positionY_sample = monochromator_to_sample * np.sin(np.radians(results['A1'])) - sample_goniometer_radius # < floor_position_y
+        # analyzer dramがfloorからはみ出る場合
+        positionY_analyzer = monochromator_to_sample * np.sin(np.radians(results['A1'])) - sample_to_analyzer * np.sin(np.radians(results['A2'] - results['A1'])) - analyzer_radius # < floor_position_y
+        # detector dramがfloorからはみ出る場合
+        positionY_detector = monochromator_to_sample * np.sin(np.radians(results['A1'])) - sample_to_analyzer * np.sin(np.radians(results['A2'] - results['A1'])) + analyzer_to_detector * np.sin(np.radians(results['A3'] - results['A2'] + results['A1'])) - detector_radius # < floor_position_y
+        positionX_detector = monochromator_to_sample * np.cos(np.radians(results['A1'])) + sample_to_analyzer * np.cos(np.radians(results['A2'] - results['A1'])) + analyzer_to_detector * np.cos(np.radians(results['A3'] - results['A2'] + results['A1'])) - detector_radius # < floor_position_x+floor_length
+        
+        # フロアからはみ出た場合、行を赤色にする
+        if (positionY_sample < floor_position_y or
+          positionY_analyzer < floor_position_y or
+          positionY_detector < floor_position_y or
+          positionX_detector > floor_position_x+floor_length):
+            tree.tag_configure("red", foreground="red")  # 'red' タグを設定
+            tree.item(item_id, tags=("red",))  # 行に 'red' タグを適用
+        
+        # hardware limitを超えた場合、行をい色にする
+        elif (round(results['C1'],4)<float(hwl2f.get()) or
+            round(results['C1'],4)>float(hwl2t.get()) or
+            round(results['A1'],4)<float(hwl3f.get()) or
+            round(results['A1'],4)>float(hwl3t.get()) or
+            round(results['C2'],4)<float(hwl4f.get()) or
+            round(results['C2'],4)>float(hwl4t.get()) or
+            round(results['A2'],4)<float(hwl5f.get()) or
+            round(results['A2'],4)>float(hwl5t.get()) or
+            round(results['C3'],4)<float(hwl6f.get()) or
+            round(results['C3'],4)>float(hwl6t.get()) or
+            round(results['A3'],4)<float(hwl7f.get()) or
+            round(results['A3'],4)>float(hwl7t.get()) or
+            round(results['mu'],4)<float(hwl8f.get()) or
+            round(results['mu'],4)>float(hwl8t.get()) or
+            round(results['nu'],4)<float(hwl9f.get()) or
+            round(results['nu'],4)>float(hwl9t.get())):
+            tree.tag_configure("red", foreground="red")  # 'red' タグを設定
+            tree.item(item_id, tags=("red",))  # 行に 'red' タグを適用
+        #tree.insert("", "end", values=values)
+        
     if fig_spec.get()==1:
         # プロット関数を呼び出し
         #plot_spectrometer_with_gif(A_sets,QE_sets)
@@ -2086,6 +2182,30 @@ def conostEscan_show_table():
     h_inc = float(ces7.get())
     k_inc = float(ces8.get())
     l_inc = float(ces9.get())
+    
+    # 先にフロアの条件を読み込み
+    # INIファイルから設定を読み込む
+    config = configparser.ConfigParser()
+    # .exe化した場合に対応する
+    if getattr(sys, 'frozen', False):
+        # .exeの場合、sys.argv[0]が実行ファイルのパスになる
+        ini_path = os.path.join(os.path.dirname(sys.argv[0]), 'config.ini')
+    else:
+        # .pyの場合、__file__がスクリプトのパスになる
+        ini_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config.read(ini_path)
+    
+    monochromator_radius = float(config['settings']['monochromator_radius'])
+    monochromator_to_sample = float(config['settings']['monochromator_to_sample'])
+    sample_goniometer_radius = float(config['settings']['sample_goniometer_radius'])
+    sample_to_analyzer = float(config['settings']['sample_to_analyzer'])
+    analyzer_radius = float(config['settings']['analyzer_radius'])
+    analyzer_to_detector = float(config['settings']['analyzer_to_detector'])
+    detector_radius = float(config['settings']['detector_radius'])
+    floor_length = float(config['settings']['floor_length'])
+    floor_width = float(config['settings']['floor_width'])
+    floor_position_x = float(config['settings']['floor_position_x'])
+    floor_position_y = float(config['settings']['floor_position_y'])
 
     # Ei or Ef fixの判定
     fixe=float(eief.get())
@@ -2093,13 +2213,12 @@ def conostEscan_show_table():
     global angletable3
     angletable3 = angle_calc3(astar,bstar,cstar,U,B,UB,bpe,bpc2,bpmu,bpnu,bp,fixe,hw_cal,h_ini,k_ini,l_ini,h_fin,k_fin,l_fin,h_inc,k_inc,l_inc)
     
-    
     A_sets = []  # A_setsリストを初期化
     QE_sets = []
     # resultsリストの各結果をTreeviewに追加
     for results in angletable3:
         values = tuple(results.values())
-        tree.insert("", "end", values=values)
+        item_id = tree.insert("", "end", values=values)
     
         # A1, A2, A3 を取得して A_sets に追加
         A1 = round(results['A1'], 4)  # 'A1'
@@ -2112,7 +2231,43 @@ def conostEscan_show_table():
         k = round(results['k'], 4)  # 'A3'
         l = round(results['l'], 4)  # 'A3'
         QE_sets.append([hw, h, k,l])
+        
+        # sample gonioがfloorからはみ出る場合
+        positionY_sample = monochromator_to_sample * np.sin(np.radians(results['A1'])) - sample_goniometer_radius # < floor_position_y
+        # analyzer dramがfloorからはみ出る場合
+        positionY_analyzer = monochromator_to_sample * np.sin(np.radians(results['A1'])) - sample_to_analyzer * np.sin(np.radians(results['A2'] - results['A1'])) - analyzer_radius # < floor_position_y
+        # detector dramがfloorからはみ出る場合
+        positionY_detector = monochromator_to_sample * np.sin(np.radians(results['A1'])) - sample_to_analyzer * np.sin(np.radians(results['A2'] - results['A1'])) + analyzer_to_detector * np.sin(np.radians(results['A3'] - results['A2'] + results['A1'])) - detector_radius # < floor_position_y
+        positionX_detector = monochromator_to_sample * np.cos(np.radians(results['A1'])) + sample_to_analyzer * np.cos(np.radians(results['A2'] - results['A1'])) + analyzer_to_detector * np.cos(np.radians(results['A3'] - results['A2'] + results['A1'])) - detector_radius # < floor_position_x+floor_length
+        
+        # フロアからはみ出た場合、行を赤色にする
+        if (positionY_sample < floor_position_y or
+          positionY_analyzer < floor_position_y or
+          positionY_detector < floor_position_y or
+          positionX_detector > floor_position_x+floor_length):
+            tree.tag_configure("red", foreground="red")  # 'red' タグを設定
+            tree.item(item_id, tags=("red",))  # 行に 'red' タグを適用
 
+        # hardware limitを超えた場合、行をい色にする
+        elif (round(results['C1'],4)<float(hwl2f.get()) or
+            round(results['C1'],4)>float(hwl2t.get()) or
+            round(results['A1'],4)<float(hwl3f.get()) or
+            round(results['A1'],4)>float(hwl3t.get()) or
+            round(results['C2'],4)<float(hwl4f.get()) or
+            round(results['C2'],4)>float(hwl4t.get()) or
+            round(results['A2'],4)<float(hwl5f.get()) or
+            round(results['A2'],4)>float(hwl5t.get()) or
+            round(results['C3'],4)<float(hwl6f.get()) or
+            round(results['C3'],4)>float(hwl6t.get()) or
+            round(results['A3'],4)<float(hwl7f.get()) or
+            round(results['A3'],4)>float(hwl7t.get()) or
+            round(results['mu'],4)<float(hwl8f.get()) or
+            round(results['mu'],4)>float(hwl8t.get()) or
+            round(results['nu'],4)<float(hwl9f.get()) or
+            round(results['nu'],4)>float(hwl9t.get())):
+            tree.tag_configure("red", foreground="red")  # 'red' タグを設定
+            tree.item(item_id, tags=("red",))  # 行に 'red' タグを適用
+        
     if fig_spec.get()==1:
         # プロット関数を呼び出し
         #plot_spectrometer_with_gif(A_sets,QE_sets)
@@ -2724,3 +2879,4 @@ pyinstaller TriAxionSim.py --noconsole
 ビルド前に前回のキャッシュと出力ディレクトリを削除
 """
 #############
+# %%
